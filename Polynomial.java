f3e05e8fe92a193576c1cfd1d15e0764e3942e55
shawnjiang1019 <shawnj1019@gmail.com>
2024-09-21 17:33:29 -0400

    Add driver

---
+++ Polynomial.java
@@ -0,0 +1,67 @@
+import java.lang.Math;
+
+
+public class Polynomial{
+    
+    double[] coefficients;
+
+    public Polynomial(){
+        this.coefficients = new double[0];
+    }
+
+    public Polynomial(double[] arr){
+        this.coefficients = new double[arr.length];
+
+        for (int i =0; i < arr.length; i++){
+            this.coefficients[i] = arr[i];
+        }  
+    }   
+
+    
+    public Polynomial add(Polynomial p2){
+        int len1 =this.coefficients.length;
+        
+        int len2 =p2.coefficients.length;
+
+        int smallest = 0;
+        double [] longArray;
+        double [] smallArray;
+        if (len1 < len2){
+            smallest = len1;
+            longArray = p2.coefficients;
+            smallArray = this.coefficients;
+        } else {
+            smallest = len2;
+            longArray = this.coefficients;
+            smallArray = p2.coefficients;
+        }
+
+
+        for (int i =0; i < smallest; i++){
+            longArray[i] = longArray[i] + smallArray[i];
+        }
+        
+        Polynomial newPoly = new Polynomial(longArray);
+
+        return newPoly;
+    }
+
+    public double evaluate(double val){
+        int answer = 0;
+
+        for (int i = 0; i < this.coefficients.length; i++){
+            answer = answer + Math.pow(val, i) * this.coefficients[i];
+        }
+
+        return answer;
+    }
+
+    public boolean hasRoot(double val){
+        int answer = this.evaluate(val);
+        if (answer == 0){
+            return true;
+        }
+
+        return false;
+    }
+}
